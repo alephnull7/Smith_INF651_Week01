7f30de4e15e4b3ab18d9f688b1c49fa7ca499859
Greg Smith <gregorymichaelsmith@protonmail.com>
2023-08-25 15:16:39 -0700

    Adding a personalized welcome message

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,12 @@
-// update content of all h1 elements
+/* 
+    a "hello world" example 
+*/
 const myHeading = document.querySelector("h1");
 myHeading.textContent = "Hello world!";
 
-// add an image changer
+/* 
+    adding an image changer 
+*/
 const myImage = document.querySelector("img");
 
 myImage.onclick = () => {
@@ -14,4 +18,36 @@ myImage.onclick = () => {
         newAttribute = "images/firefox-icon.png";
     }
     myImage.setAttribute("src", newAttribute);
+}
+
+/* 
+    adding a personalized welcome message
+
+    slightly changed from tutorial to have heading
+    content "Hello world!" by default;
+    setUserName() only called on button click
+*/
+let myButton = document.querySelector("button");
+
+function setHeading(name) {
+    myHeading.textContent = `Mozilla is cool, ${name}`;
+}
+
+function setUserName() {
+    const myName = prompt("Please enter your name.");
+    if (!myName) {
+        setUserName();
+    } else {
+        localStorage.setItem("name", myName);
+        setHeading(myName);
+    }
+}
+
+if (localStorage.getItem("name")) {
+    const storedName = localStorage.getItem("name");
+    setHeading(storedName);
+}
+
+myButton.onclick = () => {
+    setUserName();
 }
