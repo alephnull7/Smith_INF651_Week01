e928ddcfe1d5075fe306bfcf0ebee7c29ffe8ea3
Greg Smith <gregorymichaelsmith@protonmail.com>
2023-08-25 14:35:40 -0700

    Adding an image changer

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,17 @@
+// update content of all h1 elements
 const myHeading = document.querySelector("h1");
 myHeading.textContent = "Hello world!";
+
+// add an image changer
+const myImage = document.querySelector("img");
+
+myImage.onclick = () => {
+    const mySrc = myImage.getAttribute("src");
+
+    if (mySrc === "images/firefox-icon.png") {
+        newAttribute = "images/firefox2.png";
+    } else {
+        newAttribute = "images/firefox-icon.png";
+    }
+    myImage.setAttribute("src", newAttribute);
+}
